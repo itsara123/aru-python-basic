db282dd9195d49041dc5005485d504bf597ccd68
WisTiCeJEnT <wattanai.s@kbtg.tech>
2020-07-11 16:27:38 +0700

    basic lsit

--- example_list.py
+++ example_list.py
@@ -1,17 +1,17 @@
 ls = [10, 'brabra', 36,'my stringgg', '1', 1]
 print(ls)
-print(type(ls))
+for i in range(6):
+    print(ls[i])
 
-print(ls[-1])
-print(ls[-2])
-print(ls[-3])
-
-print('===============')
-for i in range(-1, -7, -1):
-    print(i, '=', ls[i])
-
-print('===============')
 for v in ls:
     print(v)
 
+print(ls)
+ls.pop(1)
+print(ls)
+
+ls.remove(10)
+print(ls)
+ls.remove('my stringgg')
+print('brabra+' in ls)
 
