39082120368252938e82485cc15beb3fa8fe2426
WisTiCeJEnT <wattanai.s@kbtg.tech>
2020-07-11 16:25:44 +0700

    quiz solution

--- quiz.py
+++ quiz.py
@@ -1,9 +1,11 @@
 ls1 = []    #เก็บ input ที่ได้รับทั้งหมด
-ls2 = []    #เก็บค่าที่มีมากกว่า 1
+ls2 = []    #เก็บเฉพาะที่ซ้ำ
 
-for i in range(6):
-    ls1.append(input())
-    # ....
+for i in range(4):
+    new_data = input()
+    if(new_data in ls1):
+        ls2.append(new_data)
+    ls1.append(new_data)
 
 print(ls1)
 print(ls2)
