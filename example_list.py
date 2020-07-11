fef865d1db25d78c256c5dc96e7a787ecc610000
WisTiCeJEnT <wattanai.s@kbtg.tech>
2020-07-11 15:20:24 +0700

    simple list

---
+++ example_list.py
@@ -0,0 +1,17 @@
+ls = [10, 'brabra', 36,'my stringgg', '1', 1]
+print(ls)
+print(type(ls))
+
+print(ls[-1])
+print(ls[-2])
+print(ls[-3])
+
+print('===============')
+for i in range(-1, -7, -1):
+    print(i, '=', ls[i])
+
+print('===============')
+for v in ls:
+    print(v)
+
+
